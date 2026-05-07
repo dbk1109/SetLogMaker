f1f62a4a91d86dfc7756948a989a84ec14aab7d2
Dabin Kim <dabin1004i@naver.com>
2026-05-08 06:01:00 +0900

    BUG FIX v3

--- script.js
+++ script.js
@@ -304,6 +304,8 @@ async function loadFFmpeg() {
 
   await ffmpeg.load({
     coreURL: "/ffmpeg/ffmpeg-core.js",
+    wasmURL: "/ffmpeg/ffmpeg-core.wasm",
+    workerURL: "/ffmpeg/ffmpeg-core.worker.js",
   });
 }
 
