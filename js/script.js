557e2bdfc4cb534d387a44e68e3896c550391ec4
Dabin Kim <dabin1004i@naver.com>
2026-05-11 22:49:00 +0900

    v2.9.9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const APP_CORE = {
   // 기준 시간은 유지 (내부 계산용)
   TIMES: [
-    "04:00",
-    "05:00",
-    "06:00",
-    "07:00",
-    "08:00",
-    "09:00",
+    "4:00",
+    "5:00",
+    "6:00",
+    "7:00",
+    "8:00",
+    "9:00",
     "10:00",
     "11:00",
     "12:00",
@@ -21,10 +21,10 @@ const APP_CORE = {
     "21:00",
     "22:00",
     "23:00",
-    "00:00",
-    "01:00",
-    "02:00",
-    "03:00",
+    "0:00",
+    "1:00",
+    "2:00",
+    "3:00",
   ],
   state: {
     user1: [],
@@ -50,10 +50,10 @@ const APP_CORE = {
     let displayHour = hour % 12;
     displayHour = displayHour === 0 ? 12 : displayHour;
 
-    const formattedHour = String(displayHour).padStart(2, "0");
+    //const formattedHour = String(displayHour).padStart(2, "0");
     const formattedMin = String(min).padStart(2, "0");
 
-    return `${formattedHour}:${formattedMin}`;
+    return `${displayHour}:${formattedMin}`;
   },
 
   initData(user) {
